0966201dfd9fb95f230e5b62571143d9dbb4be2c
Ankur <195884167+74nkur@users.noreply.github.com>
2025-02-15 16:40:20 +0530

    Solution to Check Greater Than

---
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -0,0 +1,11 @@
+#include <stido.h>
+int main{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>b){
+        printf("a is grater\n");
+    }else{
+        printf("b is grater\n");
+    }
+    return 0;
+}
