d567a03ee1ca151559c7ac6e69ae6410c0eee65d
Ankur <195884167+74nkur@users.noreply.github.com>
2025-02-16 09:26:33 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %d %c",&a,&b,&c);
+    if(c=="+"){
+        printf("Addition: %d\n",a+b);
+    }
+    else if(c=="-"){
+        printf("Subtraction: %d\n",a-b);
+    }
+    else if(c=="*"){
+        printf("Multiplication: %d\n", a*b);
+    }
+    else if(c=="/"){
+        printf("Division: %d\n",a/b);
+    }
+    else{
+        printf("Invalid")
+    }
+    return 0;
+}
